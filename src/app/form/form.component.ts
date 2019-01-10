378e88b807c1b5e6927bf5e83de7c397edc7c062
Eduardo Tordecilla <sistemas5.grupored@gmail.com>
2019-01-10 09:44:05 -0500

    avance validator desde el work

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -7,41 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
-  constructor() { 
+  constructor() {
     this.colors = [
-      {name:"#63b598"}, 
-      {name:"#ce7d78"}, 
-      {name:"#ea9e70"},
-      {name:"#a48a9e"}, 
-      {name:"#c6e1e8"}, 
-      {name:"#648177"},
-      {name:"#0d5ac1"},
-      {name:"#f205e6"},
-      {name:"#1c0365"},
-      {name:"#14a9ad"},
-      {name:"#4ca2f9"},
-      {name:"#a4e43f"},
-      {name:"#d298e2"},
-      {name:"#6119d0"},
-      {name:"#d2737d"},
-      {name:"#c0a43c"},
-      {name:"#f2510e"},
-      {name:"#651be6"},
-      {name:"#79806e"},
-      {name:"#61da5e"},
-      {name:"#cd2f00"},
+      { name: "#63b598" },
+      { name: "#ce7d78" },
+      { name: "#ea9e70" },
+      { name: "#a48a9e" },
+      { name: "#c6e1e8" },
+      { name: "#648177" },
+      { name: "#0d5ac1" },
+      { name: "#f205e6" },
+      { name: "#1c0365" },
+      { name: "#14a9ad" },
+      { name: "#4ca2f9" },
+      { name: "#a4e43f" },
+      { name: "#d298e2" },
+      { name: "#6119d0" },
+      { name: "#d2737d" },
+      { name: "#c0a43c" },
+      { name: "#f2510e" },
+      { name: "#651be6" },
+      { name: "#79806e" },
+      { name: "#61da5e" },
+      { name: "#cd2f00" },
     ];
 
     this.data = {
-      name:""
+      name: "",
+      last: "",
+      email: "",
     }
   }
 
   public colors: Array<any>
-  public data:any;
+  public data: any;
 
-  send(){
-    console.log("my data is",this.data)
+  send() {
+    console.log("my data is", this.data)
   }
 
   ngOnInit() {
